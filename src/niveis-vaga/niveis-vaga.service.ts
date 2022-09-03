9fa750bf315ebd823d056081f9cbbb0543feed7e
Matheus Andrade <matheuswcf@gmail.com>
2022-09-03 00:28:04 -0300

    Restful routes: 'GET /niveis-vaga' e 'GET /niveis-vaga/:id'

--- src/niveis-vaga/niveis-vaga.service.ts
+++ src/niveis-vaga/niveis-vaga.service.ts
@@ -1,19 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateNiveisVagaDto } from './dto/create-niveis-vaga.dto';
 import { UpdateNiveisVagaDto } from './dto/update-niveis-vaga.dto';
+import { NiveisVaga } from './entities/niveis-vaga.entity';
+import { NivelVaga} from '@prisma/client';
+
 
 @Injectable()
 export class NiveisVagaService {
+
+  constructor(private prisma: PrismaService) {}
+  
+
   create(createNiveisVagaDto: CreateNiveisVagaDto) {
     return 'This action adds a new niveisVaga';
   }
 
-  findAll() {
-    return `This action returns all niveisVaga`;
+  findAll(): Promise<NivelVaga[]> {
+    return this.prisma.nivelVaga.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} niveisVaga`;
+  async findOne(id: number): Promise<NivelVaga> {
+    const nivelVaga = await this.prisma.nivelVaga.findUnique({
+      where: { id },
+    });
+    if (!nivelVaga)
+      throw new HttpException('Nivel Vaga não encontrado', HttpStatus.NOT_FOUND);
+    return nivelVaga;
   }
 
   update(id: number, updateNiveisVagaDto: UpdateNiveisVagaDto) {
