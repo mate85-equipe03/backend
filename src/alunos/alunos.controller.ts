95c90ddc83b5ef274a9f7893ab8e11ff34389dcb
asb21 <asb2@cesar.org.br>
2022-09-11 17:10:04 -0300

    FEATURE/ Add Aluno Post route

--- src/alunos/alunos.controller.ts
+++ src/alunos/alunos.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AlunosService } from './alunos.service';
+import { Usuario as UsuarioModel} from '@prisma/client';
+
 import { CreateAlunoDto } from './dto/create-aluno.dto';
 import { UpdateAlunoDto } from './dto/update-aluno.dto';
 
@@ -7,28 +9,11 @@ import { UpdateAlunoDto } from './dto/update-aluno.dto';
 export class AlunosController {
   constructor(private readonly alunosService: AlunosService) {}
 
-  @Post()
-  create(@Body() createAlunoDto: CreateAlunoDto) {
-    return this.alunosService.create(createAlunoDto);
+  @Post('aluno')
+  async signupUser(
+    @Body() alunoData: { login: string; email: string, senha: string, siape: string},
+  ): Promise<UsuarioModel> {
+    return this.alunosService.createAluno(alunoData);
   }
 
-  @Get()
-  findAll() {
-    return this.alunosService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.alunosService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAlunoDto: UpdateAlunoDto) {
-    return this.alunosService.update(+id, updateAlunoDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.alunosService.remove(+id);
-  }
 }
--- src/alunos/alunos.service.ts
+++ src/alunos/alunos.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import {Usuario} from '@prisma/client';
 import { CreateAlunoDto } from './dto/create-aluno.dto';
 import { UpdateAlunoDto } from './dto/update-aluno.dto';
 
 @Injectable()
 export class AlunosService {
-  create(createAlunoDto: CreateAlunoDto) {
-    return 'This action adds a new aluno';
+  
+  constructor (private prisma: PrismaService ) {}
+
+
+  async createAluno (data): Promise<Usuario> {
+  
+    return this.prisma.usuario.create({
+      data : {
+        login:data['login'],
+        email:data['email'],
+        senha:data['senha'],
+        aluno: {
+        create: {matricula: data['matricula']},},
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all alunos`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} aluno`;
-  }
-
-  update(id: number, updateAlunoDto: UpdateAlunoDto) {
-    return `This action updates a #${id} aluno`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} aluno`;
-  }
 }
