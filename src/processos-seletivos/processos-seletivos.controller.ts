fd7e27dd497694d2370a089c4d57cf8b49c533a2
djairmaykon <djairmaykon@gmail.com>
2022-09-18 23:24:07 -0300

    fix[processos-seletivos]: coloca await para esperar o retorno dos arrays na função findAllfiltrado

--- src/processos-seletivos/processos-seletivos.controller.ts
+++ src/processos-seletivos/processos-seletivos.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ProcessosSeletivosService } from './processos-seletivos.service';
 import { CreateProcessosSeletivoDto } from './dto/create-processos-seletivo.dto';
 import { UpdateProcessosSeletivoDto } from './dto/update-processos-seletivo.dto';
+import { ProcessoSeletivo } from '@prisma/client';
 
 @Controller('processos-seletivos')
 export class ProcessosSeletivosController {
-  constructor(private readonly processosSeletivosService: ProcessosSeletivosService) {}
+  constructor(
+    private readonly processosSeletivosService: ProcessosSeletivosService,
+  ) {}
 
   @Post()
   create(@Body() createProcessosSeletivoDto: CreateProcessosSeletivoDto) {
@@ -18,13 +29,17 @@ export class ProcessosSeletivosController {
   //}
 
   @Get()
-  findAllfiltrado() {
-    var processos_arquivados = this.processosSeletivosService.findarquivados();
-    var processos_ativos = this.processosSeletivosService.findativos();
-    var result = {"editais":{"em_andamento": processos_ativos,
-      "arquivados":processos_arquivados, 
-  }};
-    return result
+  async findAllfiltrado() {
+    const processos_arquivados =
+      await this.processosSeletivosService.findarquivados();
+    const processos_ativos = await this.processosSeletivosService.findativos();
+    const result = {
+      editais: {
+        em_andamento: processos_ativos,
+        arquivados: processos_arquivados,
+      },
+    };
+    return result;
   }
 
   @Get(':id')
@@ -33,14 +48,18 @@ export class ProcessosSeletivosController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProcessosSeletivoDto: UpdateProcessosSeletivoDto) {
-    return this.processosSeletivosService.update(+id, updateProcessosSeletivoDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateProcessosSeletivoDto: UpdateProcessosSeletivoDto,
+  ) {
+    return this.processosSeletivosService.update(
+      +id,
+      updateProcessosSeletivoDto,
+    );
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.processosSeletivosService.remove(+id);
   }
-
-
 }
