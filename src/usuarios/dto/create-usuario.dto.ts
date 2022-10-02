8546d31b1b8c5403b4b3bf0bfd0e2574e69bf4ef
Fernando Gonçalves <fernandosgonc@gmail.com>
2022-10-01 23:29:44 -0300

    Add decorators de validação em UsuariosDTO

--- src/usuarios/dto/create-usuario.dto.ts
+++ src/usuarios/dto/create-usuario.dto.ts
@@ -1,6 +1,18 @@
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+
 export class CreateUsuarioDto {
+  @IsNotEmpty()
   login: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @Length(6, 8)
   senha: string;
-  telefone:string;
+
+  @IsNotEmpty()
+  @Length(11, 15)
+  telefone: string;
 }
