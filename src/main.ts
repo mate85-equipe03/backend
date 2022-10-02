4c0e3e2ab8848757d3036b54cb9544c6901e90f0
Fernando Gonçalves <fernandosgonc@gmail.com>
2022-10-01 23:27:50 -0300

    Adiciona Pipe global de validações

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -5,6 +6,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe());
+
   const config = new DocumentBuilder()
     .setTitle('API')
     .setDescription('The backend API description')
