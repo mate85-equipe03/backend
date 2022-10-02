4082eb92c3dda865b96391e29924e15e4de08914
Fernando Gonçalves <fernandosgonc@gmail.com>
2022-10-01 23:29:24 -0300

    AlunosController utiliza novo AlunosDTO

--- src/alunos/alunos.controller.ts
+++ src/alunos/alunos.controller.ts
@@ -10,13 +10,14 @@ import {
 import { AlunosService } from './alunos.service';
 import { Usuario as UsuarioModel } from '@prisma/client';
 import { CreateUsuarioDto } from 'src/usuarios/dto/create-usuario.dto';
+import { CreateAlunoDto } from './dto/create-aluno.dto';
 
 @Controller('alunos')
 export class AlunosController {
   constructor(private readonly alunosService: AlunosService) {}
 
   @Post()
-  async signupUser(@Body() alunoData: CreateUsuarioDto): Promise<UsuarioModel> {
+  async signupUser(@Body() alunoData: CreateAlunoDto): Promise<UsuarioModel> {
     return this.alunosService.createAluno(alunoData);
   }
 }
