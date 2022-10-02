c48fb47ca90e6fd8773cdf8bf9059ab4a16519f5
Fernando Gonçalves <fernandosgonc@gmail.com>
2022-10-01 23:28:44 -0300

    CreateAlunoDTO extende UsuarioDTO e add decorators de validação

--- src/alunos/dto/create-aluno.dto.ts
+++ src/alunos/dto/create-aluno.dto.ts
@@ -1,1 +1,19 @@
-export class CreateAlunoDto {}
+import { IsNotEmpty, IsNumber, IsNumberString, Length } from 'class-validator';
+import { CreateUsuarioDto } from 'src/usuarios/dto/create-usuario.dto';
+
+export class CreateAlunoDto extends CreateUsuarioDto {
+  @IsNotEmpty()
+  @Length(10)
+  @IsNumberString()
+  matricula: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  semestre_pgcomp: number;
+
+  @IsNotEmpty()
+  curso: string;
+
+  @IsNotEmpty()
+  lattes_link: string;
+}
