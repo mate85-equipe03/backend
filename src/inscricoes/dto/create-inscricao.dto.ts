f23bea340a85901f7683b27da90f582fcf398f5f
Fernando Gonçalves <fernandosgonc@gmail.com>
2022-10-02 13:42:26 -0300

    InscricaoDTO validacao inicial de campos

--- src/inscricoes/dto/create-inscricao.dto.ts
+++ src/inscricoes/dto/create-inscricao.dto.ts
@@ -1,5 +1,21 @@
+import { IsNotEmpty, IsNumberString } from 'class-validator';
+
 export class CreateInscricaoDto {
+  @IsNotEmpty()
   url_lattes: string;
+
+  @IsNotEmpty()
   url_enade: string;
+
+  @IsNotEmpty()
+  @IsNumberString()
   processo_seletivo_id: number;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  status_inscricao: number;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  aluno_id: number;
 }
