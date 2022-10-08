b4c826ff59091bcbf92f8132147fd8ef7eb62bf5
asb21 <asb2@cesar.org.br>
2022-10-08 14:12:40 -0300

    feature/Implementa o Role do professor pra visualizar as inscrições

--- src/processos-seletivos/processos-seletivos.controller.ts
+++ src/processos-seletivos/processos-seletivos.controller.ts
@@ -6,13 +6,17 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards
 } from '@nestjs/common';
 import { ProcessosSeletivosService } from './processos-seletivos.service';
 import { CreateProcessosSeletivoDto } from './dto/create-processos-seletivo.dto';
 import { UpdateProcessosSeletivoDto } from './dto/update-processos-seletivo.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { InscricoesService } from 'src/inscricoes/inscricoes.service';
-import { int } from 'aws-sdk/clients/datapipeline';
+import { JwtAuthGuard } from 'src/autenticacao/guards/jwt-auth.guard';
+import { Roles } from 'src/autenticacao/decorators/roles.decorator';
+import { Role, TipoHistorico } from '@prisma/client';
+import { RolesGuard } from 'src/autenticacao/guards/roles.guard';
 
 @Controller('processos-seletivos')
 export class ProcessosSeletivosController {
@@ -48,6 +52,8 @@ export class ProcessosSeletivosController {
     return this.processosSeletivosService.findOne(+id);
   }
 
+  @Roles(Role.PROFESSOR)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get(':id/inscricoes')
   async findinscricoes(@Param('id') id: string) {
   const processo = await this.processosSeletivosService.findOne(+id);
@@ -55,6 +61,8 @@ export class ProcessosSeletivosController {
   return inscricoes
   }
 
+  @Roles(Role.PROFESSOR)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get(':id/inscricoes/:id2')
   async findinscricao(@Param('id') id: string,@Param('id2') id2: string) {
   const processo = await this.processosSeletivosService.findOne(+id);
