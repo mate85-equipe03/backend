006786a095c30c0d4eae795218e2ee4fa20ef504
Fernando Gonçalves <fernandosgonc@gmail.com>
2022-10-09 15:06:59 -0300

    Usuarios: Validacao campos obrigatorios

--- src/usuarios/dto/create-usuario.dto.ts
+++ src/usuarios/dto/create-usuario.dto.ts
@@ -1,6 +1,19 @@
+import { IsEmail, IsNotEmpty, IsNumberString, Length } from 'class-validator';
+
 export class CreateUsuarioDto {
+  @IsNumberString()
+  @IsNotEmpty()
+  @Length(7, 9)
   login: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsNotEmpty()
   senha: string;
-  telefone:string;
+
+  @IsNumberString()
+  @Length(11)
+  telefone: string;
 }
