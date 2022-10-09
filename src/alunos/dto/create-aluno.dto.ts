9c8f809d454f0cd697d2c324dc714ec1cd0e73b7
Fernando Gonçalves <fernandosgonc@gmail.com>
2022-10-09 15:09:37 -0300

    Aluno: valida campos obrigatorios

--- src/alunos/dto/create-aluno.dto.ts
+++ src/alunos/dto/create-aluno.dto.ts
@@ -1,1 +1,14 @@
-export class CreateAlunoDto {}
+import { IsInt, IsNotEmpty, IsUrl } from 'class-validator';
+import { CreateUsuarioDto } from 'src/usuarios/dto/create-usuario.dto';
+
+export class CreateAlunoDto extends CreateUsuarioDto {
+  @IsInt()
+  semestre_pgcomp: number;
+
+  @IsNotEmpty()
+  curso: string;
+
+  @IsUrl()
+  @IsNotEmpty()
+  lates_link: string;
+}
