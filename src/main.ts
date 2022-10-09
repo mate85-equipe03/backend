765028ced3e8bd5d7b6f56eb38e8345ec47b3636
Fernando Gonçalves <fernandosgonc@gmail.com>
2022-10-09 15:09:08 -0300

    Add Pipe global

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -12,6 +13,8 @@ async function bootstrap() {
     .addTag('bolsas')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+
+  app.useGlobalPipes(new ValidationPipe());
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 3000);
