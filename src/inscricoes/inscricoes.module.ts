1f6eb6b293c26461304f51180d9a4ef603dca4b9
asb21 <asb2@cesar.org.br>
2022-10-20 17:03:29 -0300

    feature/Add Email nas inscricoes de sucesso

--- src/inscricoes/inscricoes.module.ts
+++ src/inscricoes/inscricoes.module.ts
@@ -6,7 +6,8 @@ import { AlunosModule } from 'src/alunos/alunos.module';
 import { SpacesModule } from 'src/SpacesModule/spaces.module';
 import { HistoricoModule } from 'src/historico/historico.module';
 import { ProducaoCientificaModule } from 'src/producao-cientifica/producao-cientifica.module';
-
+import { MailerModule } from '@nestjs-modules/mailer';
+import { UsuariosModule } from 'src/usuarios/usuarios.module';
 @Module({
   imports: [
     forwardRef(() => ProcessosSeletivosModule),
@@ -14,7 +15,17 @@ import { ProducaoCientificaModule } from 'src/producao-cientifica/producao-cient
     SpacesModule,
     HistoricoModule,
     ProducaoCientificaModule,
-  ],
+    UsuariosModule,
+    MailerModule.forRoot({
+      transport:{
+        host:'localhost',
+        port:1025
+  
+      },
+      defaults:{
+        from:'no-reply@postgress.com'
+      }
+    })],
   controllers: [InscricoesController],
   providers: [InscricoesService],
   exports: [InscricoesService],
