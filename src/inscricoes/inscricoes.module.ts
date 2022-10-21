ce3312070531b57d850e72e1e2745449f7b67644
Matheus Andrade <matheuswcf@gmail.com>
2022-10-21 04:19:53 -0300

    Revert "refactor/muda o atributo do processo seletivo para string"

--- src/inscricoes/inscricoes.module.ts
+++ src/inscricoes/inscricoes.module.ts
@@ -6,8 +6,7 @@ import { AlunosModule } from 'src/alunos/alunos.module';
 import { SpacesModule } from 'src/SpacesModule/spaces.module';
 import { HistoricoModule } from 'src/historico/historico.module';
 import { ProducaoCientificaModule } from 'src/producao-cientifica/producao-cientifica.module';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { UsuariosModule } from 'src/usuarios/usuarios.module';
+
 @Module({
   imports: [
     forwardRef(() => ProcessosSeletivosModule),
@@ -15,17 +14,7 @@ import { UsuariosModule } from 'src/usuarios/usuarios.module';
     SpacesModule,
     HistoricoModule,
     ProducaoCientificaModule,
-    UsuariosModule,
-    MailerModule.forRoot({
-      transport:{
-        host:'localhost',
-        port:1025
-  
-      },
-      defaults:{
-        from:'no-reply@postgress.com'
-      }
-    })],
+  ],
   controllers: [InscricoesController],
   providers: [InscricoesService],
   exports: [InscricoesService],
