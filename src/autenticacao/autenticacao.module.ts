2d69393e963ce66b26315c3a699b80ac2c5d5b18
Matheus Andrade <matheuswcf@gmail.com>
2022-10-23 21:13:01 -0300

    Entrega mais informações do usuario logado (aluno, professor ou root).

--- src/autenticacao/autenticacao.module.ts
+++ src/autenticacao/autenticacao.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsuariosModule } from 'src/usuarios/usuarios.module';
+import { AlunosModule } from 'src/alunos/alunos.module';
+import { ProfessoresModule } from 'src/professores/professores.module';
 import { AutenticacaoController } from './autenticacao.controller';
 import { AutenticacaoService } from './autenticacao.service';
 import { OptionalJwtAuthGuard } from './guards/optional-jwt-auth.guard';
@@ -13,6 +15,8 @@ import { LocalStrategy } from './strategies/local.strategy';
   controllers: [AutenticacaoController],
   imports: [
     UsuariosModule,
+    AlunosModule,
+    ProfessoresModule,
     PassportModule,
     JwtModule.registerAsync({
       useFactory: () => ({
--- src/autenticacao/autenticacao.service.ts
+++ src/autenticacao/autenticacao.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsuariosService } from 'src/usuarios/usuarios.service';
+import { AlunosService } from 'src/alunos/alunos.service';
+import { ProfessoresService } from 'src/professores/professores.service';
 import * as bcrypt from 'bcrypt';
+import { Role } from '@prisma/client';
 
 @Injectable()
 export class AutenticacaoService {
   constructor(
     private usuariosService: UsuariosService,
     private jwtService: JwtService,
+    private alunosService: AlunosService,
+    private professoresService: ProfessoresService,
   ) {}
 
   async validateUser(login: string, senha: string): Promise<any> {
@@ -28,11 +33,32 @@ export class AutenticacaoService {
       login: usuario.login,
       userId: usuario.id,
     };
+
+    var matricula = "";
+    var nome = "";
+
+    if (usuario.role == Role.ALUNO){
+      const aluno = await this.alunosService.findAlunoByUserId(usuario.id); 
+      matricula = aluno.matricula;
+      nome = aluno.nome;
+    }else if (usuario.role == Role.PROFESSOR){
+      const professor = await this.professoresService.findProfessorByUserId(usuario.id); 
+      matricula = professor.siape
+      nome = professor.nome;
+    }else{
+      matricula = "ROOT";
+      nome = "ROOT";
+    }
+
     return {
       access_token: this.jwtService.sign(payload),
       user: {
         role: usuario.role,
         id: usuario.id,
+        email: usuario.email,
+        telefone: usuario.telefone,
+        nome: nome,
+        matricula: matricula,
       },
     };
   }
