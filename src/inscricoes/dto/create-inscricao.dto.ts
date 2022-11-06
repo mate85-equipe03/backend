bef877a7b92565b49d5ed68cf2728bca557a1b2c
Matheus Andrade <matheuswcf@gmail.com>
2022-11-05 23:01:19 -0300

    notas em historicos e enade (na gambiarra, mas foi).

--- src/inscricoes/dto/create-inscricao.dto.ts
+++ src/inscricoes/dto/create-inscricao.dto.ts
@@ -1,4 +1,7 @@
 export class CreateInscricaoDto {
   url_enade: string;
+  nota_enade: string;
   processo_seletivo_id: number;
+  nota_historico_graduacao: string;
+  nota_historico_posgraduacao: string;
 }
