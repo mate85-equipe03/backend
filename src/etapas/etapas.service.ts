4caade91dce5d4efc866fcc50ce8400d983582b5
asb21 <asb2@cesar.org.br>
2022-11-17 22:31:12 -0300

    feature/cria as rotas que adiciona um processo seletivo, adiciona uma etapa a um processo seletivo e edita uma etapa

--- src/etapas/etapas.service.ts
+++ src/etapas/etapas.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEtapaDto } from './dto/create-etapa.dto';
 import { UpdateEtapaDto } from './dto/update-etapa.dto';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+
 
 @Injectable()
 export class EtapasService {
-  create(createEtapaDto: CreateEtapaDto) {
-    return 'This action adds a new etapa';
+constructor(private prisma: PrismaService) {}
+  
+create(data,id) {
+    return this.prisma.etapa.create({
+      data: {
+        processo_seletivo_id: parseInt(id),
+        name: data.name,
+        data_inicio: new Date(data.data_inicio),
+        data_fim: new Date(data.data_fim),
+      },
+    });
   }
 
   findAll() {
@@ -16,8 +28,15 @@ export class EtapasService {
     return `This action returns a #${id} etapa`;
   }
 
-  update(id: number, updateEtapaDto: UpdateEtapaDto) {
-    return `This action updates a #${id} etapa`;
+  update(id: number, data) {
+    return this.prisma.etapa.update({
+      where: { id: id },
+      data: {
+        name: data.name,
+        data_inicio: new Date(data.data_inicio),
+        data_fim: new Date(data.data_fim),
+      },
+    });
   }
 
   remove(id: number) {
--- src/processos-seletivos/dto/create-processos-seletivo.dto.ts
+++ src/processos-seletivos/dto/create-processos-seletivo.dto.ts
@@ -1,5 +1,6 @@
 export class CreateProcessosSeletivoDto {
 titulo:string;
+semestre:string;
 descricao:string;
 edital_url:string;
 }
