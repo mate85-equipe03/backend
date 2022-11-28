6be46b2db6c1e675a06bbe2a62f17af5699a15f7
asb21 <asb2@cesar.org.br>
2022-11-28 17:04:54 -0300

    Adiciona a categoria no find by inscricao

--- src/inscricoes/inscricoes.service.ts
+++ src/inscricoes/inscricoes.service.ts
@@ -75,7 +75,11 @@ export class InscricoesService {
       include: {
         Historico: true,
         aluno: true,
-        producoes: true,
+        producoes: {
+          include: {
+            categorias_producao: true,
+          },
+        },
         revisor:true,
         auditor:true,
       },
