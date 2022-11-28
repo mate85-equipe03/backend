2a61124a3c26819787d7b77201f9bbd22b083e75
Matheus Andrade <matheuswcf@gmail.com>
2022-11-27 22:36:34 -0300

    Corrige metodo de etapa-atual.

--- src/processos-seletivos/processos-seletivos.controller.ts
+++ src/processos-seletivos/processos-seletivos.controller.ts
@@ -79,12 +79,23 @@ export class ProcessosSeletivosController {
   @Get(':id/etapa-atual')
   async getEtapaAtual(@Param('id') id: string, @Request() req) {
     const processo = await this.processosSeletivosService.findOne(+id);
-
     if(processo.resultado_liberado){
       return await this.etapasService.findEtapaResultado(+id);
     }
     else{
-      return await this.etapasService.findAtual(+id);
+      const etapa = await this.etapasService.findAtual(+id);
+      if(etapa.name == "Resultado Final"){
+        return {
+          "id": 999999,
+          "processo_seletivo_id": id,
+          "name": "Resultado Final em breve",
+          "data_inicio": "",
+          "data_fim": "",          
+        };
+      }
+      else{
+        return etapa;
+      }
     }
     
   }
